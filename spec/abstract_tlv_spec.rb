e0bd91a5569afb641af2eeb3c6be6c0cd5bf2924
lemontree55 <lenontree@proton.me>
2024-11-24 16:06:10 +0100

    Add specs on AbstractTLV

--- spec/abstract_tlv_spec.rb
+++ spec/abstract_tlv_spec.rb
@@ -2,15 +2,21 @@
 
 require_relative 'spec_helper'
 
-TestEnumTLV = BinStruct::AbstractTLV.create
-TestEnumTLV.define_type_enum 'one' => 1, 'two' => 2
+module BSTLVSpec
+  TestEnumTLV = BinStruct::AbstractTLV.create
+  TestEnumTLV.define_type_enum 'one' => 1, 'two' => 2
 
-TestNoEnumTLV = BinStruct::AbstractTLV.create(type_class: BinStruct::Int8)
+  TestNoEnumTLV = BinStruct::AbstractTLV.create(type_class: BinStruct::Int8)
+
+  TestDefaultTypeTLV = BinStruct::AbstractTLV.create(type_class: BinStruct::Int8)
+  TestDefaultTypeTLV.define_type_default(127)
+end
 
 module BinStruct
   RSpec.describe AbstractTLV do
     describe '.create' do
       let(:tlv_class) { AbstractTLV.create }
+
       it 'returns a class, subclass of BinStruct::Struct' do
         expect(tlv_class).to be_a(Class)
         expect(tlv_class).to be < BinStruct::Struct
@@ -44,7 +50,7 @@ module BinStruct
       end
 
       it 'raises when called on a subclass' do
-        expect { TestEnumTLV.create }.to raise_error(Error)
+        expect { BSTLVSpec::TestEnumTLV.create }.to raise_error(Error)
       end
 
       it 'accepts an aliases argument' do
@@ -55,10 +61,48 @@ module BinStruct
         expect(tlv).to respond_to(:code)
         expect(tlv).to respond_to(:code=)
       end
+
+      it 'accepts setting length size in length' do
+        tlv_class = AbstractTLV.create(attr_in_length: 'LV')
+        tlv = tlv_class.new
+        expect(tlv.length).to eq(1)
+      end
+
+      it 'accepts setting type size in length' do
+        tlv_class = AbstractTLV.create(attr_in_length: 'TLV')
+        tlv = tlv_class.new
+        expect(tlv.length).to eq(2)
+      end
+
+      it 'raises on bad value in attr_in_lengtg' do
+        expect { AbstractTLV.create(attr_in_length: 'A') }.to raise_error
+      end
+
+      it 'accepts setting attributes in different order' do
+        tlv_class = AbstractTLV.create(attr_order: 'LVT')
+        tlv = tlv_class.new(type: 1, value: 'abc')
+        expect(tlv.to_s).to eq(binary("\x03abc\x01"))
+      end
+
+      it 'raises if attr_order is not set properly' do
+        expect { AbstractTLV.create(attr_order: 'TTT') }.to raise_error
+        expect { AbstractTLV.create(attr_order: 'TLT') }.to raise_error
+        expect { AbstractTLV.create(attr_order: 'TL') }.to raise_error
+        expect { AbstractTLV.create(attr_order: 'TLA') }.to raise_error
+        expect { AbstractTLV.create(attr_order: 'ABC') }.to raise_error
+        expect { AbstractTLV.create(attr_order: 'V') }.to raise_error
+      end
     end
 
     context 'use of instance of generated class' do
       let(:tlv) { AbstractTLV.create(type_class: Int16, length_class: Int16).new }
+
+      describe '#initialize' do
+        it 'sets type with value set by .define_type_default' do
+          tlv = BSTLVSpec::TestDefaultTypeTLV.new
+          expect(tlv.type).to eq(127)
+        end
+      end
 
       describe '#read' do
         it 'reads a TLV from a binary string' do
@@ -72,13 +116,13 @@ module BinStruct
 
       describe '#human_type' do
         it 'returns human readable type, if type is an Enum' do
-          tlv = TestEnumTLV.new(type: 'one')
+          tlv = BSTLVSpec::TestEnumTLV.new(type: 'one')
           expect(tlv.type).to eq(1)
           expect(tlv.human_type).to eq('one')
         end
 
         it 'returns a string, if type is not an enum' do
-          tlv = TestNoEnumTLV.new(type: 3)
+          tlv = BSTLVSpec::TestNoEnumTLV.new(type: 3)
           expect(tlv.type).to eq(3)
           expect(tlv.human_type).to eq('3')
         end
@@ -86,12 +130,12 @@ module BinStruct
 
       describe '#to_human' do
         it 'returns a string (type is an Enum)' do
-          tlv = TestEnumTLV.new(type: 1, value: 'abcdef')
+          tlv = BSTLVSpec::TestEnumTLV.new(type: 1, value: 'abcdef')
           expect(tlv.to_human).to eq('type:one,length:6,value:"abcdef"')
         end
 
         it 'returns a string (type is  not an Enum)' do
-          tlv = TestNoEnumTLV.new
+          tlv = BSTLVSpec::TestNoEnumTLV.new
           expect(tlv.to_human).to eq('type:0,length:0,value:""')
           tlv.type = 156
           tlv.value = 'abcd'
@@ -109,6 +153,15 @@ module BinStruct
           tlv = AbstractTLV.create(type_class: Int16, length_class: Int16, attr_in_length: 'TLV').new
           tlv.value = 'abcdef'
           expect(tlv.length).to eq(10)
+        end
+
+        it 'accepts an object if this object is of value class' do
+          tlv_class = AbstractTLV.create(value_class: Int32)
+          tlv = tlv_class.new
+          value_obj = Int32.new(value: 42)
+          tlv.value = value_obj
+          expect(tlv.value).to eq(42)
+          expect(tlv[:value]).to equal(value_obj)
         end
       end
 
